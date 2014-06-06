3a8c8c356d2175bbbd724fc025ca904d646b3ded
Will Ware <wware@alum.mit.edu>
2014-06-06 15:27:14 -0400

    Add boolean logic parser with unary operator, refactor a bit.

--- lrparser.groovy
+++ lrparser.groovy
@@ -30,6 +30,8 @@ abstract class LRParser {
 		Object action() { }
 	}
 
+	String inputString
+
 	def operators = []
 	def operandStack = []
 	def operatorStack = []
@@ -46,21 +48,32 @@ abstract class LRParser {
 		// overload me
 	}
 
-	/**
-	 *   Parse things that aren't operators.
-	 */
-	abstract String lexToken(String str)
+	abstract Operand parseAtom()
 
-	def lexOperatorToken(string) {
-		for (Token token : operators) {
-			def matches = string =~ token.getRegex()
-			if (matches.size() == 1) {
-				consumeToken(token)
-				string = string.substring(matches[0].size())
-				return [true, string]
+	void lexToken() {
+		def found = lexOperatorToken()
+		if (!found) {
+			found = parseAtom()
+			if (found != null) {
+				consumeToken(found)
+			} else {
+				// skipping whitespace
+				assert inputString.substring(0, 1) == ' '
+				inputString = inputString.substring(1)
 			}
 		}
-		return [false, string]
+	}
+
+	boolean lexOperatorToken() {
+		for (Token token : operators) {
+			def matches = inputString =~ token.getRegex()
+			if (matches.size() == 1) {
+				consumeToken(token)
+				inputString = inputString.substring(matches[0].size())
+				return true
+			}
+		}
+		return false
 	}
 
 	def doOneOp() {
@@ -95,8 +108,9 @@ abstract class LRParser {
 	}
 
 	def parse(str) {
-		while (str.size() > 0) {
-			str = lexToken(str)
+		inputString = str
+		while (inputString.size() > 0) {
+			lexToken()
 		}
 		while (operatorStack.size > 0) {
 			doOneOp()
@@ -107,10 +121,21 @@ abstract class LRParser {
 
 /* = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
 
-class ArithmeticParser extends LRParser {
+class LogicParser extends LRParser {
 
-	static class Number extends LRParser.Operand {
-		Number(value) { super(value) }
+	static class Boolean extends LRParser.Operand {
+		Boolean(value) { super(value) }
+	}
+
+	Operand parseAtom() {
+		def retval = null
+		def matches = inputString =~ ~"^(TRUE|FALSE)"
+		if (matches.size() == 1) {
+			def value = matches[0][0]
+			retval = new Boolean(value == 'TRUE')
+			inputString = inputString.substring(value.size())
+		}
+		retval
 	}
 
 	class BinaryOperator extends LRParser.Operator {
@@ -122,20 +147,73 @@ class ArithmeticParser extends LRParser {
 		}
 	}
 
-	String lexToken(String string) {
-		def (found, str) = lexOperatorToken(string)
-		if (!found) {
-			def matches = str =~ ~"^[0-9.]+"
-			if (matches.size() == 1) {
-				consumeToken(new Number(Double.parseDouble(matches[0])))
-				str = str.substring(matches[0].size())
-			} else {
-				// skipping whitespace
-				assert str.substring(0, 1) == ' '
-				str = str.substring(1)
-			}
+	class UnaryOperator extends LRParser.Operator {
+		UnaryOperator(Integer precedence, Pattern regex, Closure op) { super(precedence, regex, op) }
+		Object action() {
+			def x = operandStack.pop().value
+			op(x)
 		}
-		str
+	}
+
+	void additonalTokens() {
+		operators << new BinaryOperator(1, ~"^AND", { x, y -> x && y })
+		operators << new BinaryOperator(2, ~"^OR",  { x, y -> x || y })
+		operators << new UnaryOperator(3,  ~"^NOT", { x -> !x })
+	}
+}
+
+assert new LogicParser().parse("TRUE AND TRUE") == true
+assert new LogicParser().parse("TRUE AND FALSE OR FALSE AND TRUE") == false
+assert new LogicParser().parse("TRUE AND FALSE OR TRUE AND TRUE") == true
+assert new LogicParser().parse("TRUE AND TRUE OR FALSE AND TRUE") == true
+assert new LogicParser().parse("TRUE AND TRUE OR TRUE AND TRUE") == true
+assert new LogicParser().parse("FALSE AND (FALSE OR FALSE)") == false
+assert new LogicParser().parse("FALSE AND (FALSE OR TRUE)") == false
+assert new LogicParser().parse("FALSE AND (TRUE OR FALSE)") == false
+assert new LogicParser().parse("FALSE AND (TRUE OR TRUE)") == false
+assert new LogicParser().parse("TRUE AND (FALSE OR FALSE)") == false
+assert new LogicParser().parse("TRUE AND (FALSE OR TRUE)") == true
+assert new LogicParser().parse("TRUE AND (TRUE OR FALSE)") == true
+assert new LogicParser().parse("TRUE AND (TRUE OR TRUE)") == true
+assert new LogicParser().parse("TRUE AND NOT TRUE") == false
+assert new LogicParser().parse("TRUE AND NOT FALSE") == true
+assert new LogicParser().parse("TRUE AND FALSE OR NOT FALSE AND TRUE") == true
+assert new LogicParser().parse("TRUE AND FALSE OR NOT TRUE AND TRUE") == false
+assert new LogicParser().parse("NOT (TRUE AND NOT TRUE)") == true
+assert new LogicParser().parse("NOT (TRUE AND NOT FALSE)") == false
+assert new LogicParser().parse("FALSE AND NOT (TRUE AND NOT TRUE)") == false
+assert new LogicParser().parse("TRUE AND NOT (TRUE AND NOT TRUE)") == true
+
+// This should not work, but it does. "TRUE NOT" should not be grammatical.
+// That's because I don't have a real BNF, just this tinkering with stacks.
+assert new LogicParser().parse("TRUE AND TRUE NOT") == false
+
+/* = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
+
+class ArithmeticParser extends LRParser {
+
+	static class Number extends LRParser.Operand {
+		Number(value) { super(value) }
+	}
+
+	Operand parseAtom() {
+		def retval = null
+		def matches = inputString =~ ~"^[0-9]+(\\.[0-9]*)?"
+		if (matches.size() == 1) {
+			def value = matches[0][0]
+			retval = new Number(Double.parseDouble(value))
+			inputString = inputString.substring(value.size())
+		}
+		retval
+	}
+
+	class BinaryOperator extends LRParser.Operator {
+		BinaryOperator(Integer precedence, Pattern regex, Closure op) { super(precedence, regex, op) }
+		Object action() {
+			def y = operandStack.pop().value
+			def x = operandStack.pop().value
+			op(x, y)
+		}
 	}
 
 	void additonalTokens() {
@@ -147,7 +225,7 @@ class ArithmeticParser extends LRParser {
 }
 
 assert new ArithmeticParser().parse("2 * 3 + 4 / 5") == 6.8
-assert new ArithmeticParser().parse("2 * (3 + 4) / 5") == 2.8
+assert new ArithmeticParser().parse("2.93 * (3 + 4) / 5") == 4.102
 
 /* = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
 
