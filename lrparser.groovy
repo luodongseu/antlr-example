a8a780b6f507fb2e1fdd199e0df234851fefc2fd
Will Ware <wware@alum.mit.edu>
2014-06-06 11:41:03 -0400

    clean up and get parens working, before working on unary operators

--- lrparser.groovy
+++ lrparser.groovy
@@ -2,42 +2,57 @@
  *  Groovy parser+lexer for simple arithmetic expressions
  */
 
-interface Token {
-	String getRegex()
-}
-
-class Operand implements Token {
-	String getRegex() {
-		""
-	}
-	Object value
-	Operand(value) {
-		this.value = value
-	}
-}
-
-class Number extends Operand {
-	String toString() { "" + value }
-	static String getRegex() { ~"^[0-9.]+" }
-	Number(value) {
-		super(value)
-	}
-}
-
-abstract class Operator implements Token {
-	abstract int getPrecedence()
-	abstract Object action(x, y)
-}
+import java.util.regex.Pattern;
 
 abstract class LRParser {
-	def tokens = []
+
+	interface Token {}
+
+	static class Operand implements Token {
+		Object value
+		Operand(value) { this.value = value }
+	}
+
+	static abstract class Operator implements Token {
+		Integer precedence
+		String regex
+		Closure op
+		Operator(Integer precedence, Pattern regex, Closure op) {
+			this.precedence = precedence
+			this.regex = regex
+			this.op = op
+		}
+		abstract Object action()
+	}
+
+	class SpecialCaseOperator extends Operator {
+		SpecialCaseOperator(Pattern regex) { super(0, regex, null) }
+		Object action() { }
+	}
+
+	def operators = []
 	def operandStack = []
 	def operatorStack = []
+	def leftParen = new SpecialCaseOperator(~"^\\(")
+	def rightParen = new SpecialCaseOperator(~"^\\)")
 
+	LRParser() {
+		operators << leftParen
+		operators << rightParen
+		additonalTokens()
+	}
+
+	void additonalTokens() {
+		// overload me
+	}
+
+	/**
+	 *   Parse things that aren't operators.
+	 */
 	abstract String lexToken(String str)
 
 	def lexOperatorToken(string) {
-		for (Token token : tokens) {
+		for (Token token : operators) {
 			def matches = string =~ token.getRegex()
 			if (matches.size() == 1) {
 				consumeToken(token)
@@ -53,14 +68,22 @@ abstract class LRParser {
 			assert operatorStack.size == 0
 			return
 		}
-		def y = operandStack.pop().value
-		def op2 = operatorStack.pop()
-		operandStack << new Operand(op2.action(operandStack.pop().value, y))
+		Operator op = operatorStack.pop()
+		operandStack << new Operand(op.action())
 	}
 
 	def consumeToken(tkn) {
-		if (tkn instanceof Operator) {
-			while (operatorStack.size > 0 && 
+		if (tkn == leftParen) {
+			operatorStack << leftParen
+		} else if (tkn == rightParen) {
+			while (operatorStack.size > 0 &&
+					operatorStack[operatorStack.size() - 1] != leftParen) {
+				doOneOp()
+			}
+			operatorStack.pop()
+		} else if (tkn instanceof Operator) {
+			while (operatorStack.size > 0 &&
+					operatorStack[operatorStack.size() - 1] != leftParen &&
 					operatorStack[operatorStack.size() - 1].getPrecedence() >= tkn.getPrecedence()) {
 				doOneOp()
 			}
@@ -82,14 +105,29 @@ abstract class LRParser {
 	}
 }
 
+/* = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
+
 class ArithmeticParser extends LRParser {
+
+	static class Number extends LRParser.Operand {
+		Number(value) { super(value) }
+	}
+
+	class BinaryOperator extends LRParser.Operator {
+		BinaryOperator(Integer precedence, Pattern regex, Closure op) { super(precedence, regex, op) }
+		Object action() {
+			def y = operandStack.pop().value
+			def x = operandStack.pop().value
+			op(x, y)
+		}
+	}
 
 	String lexToken(String string) {
 		def (found, str) = lexOperatorToken(string)
 		if (!found) {
-			def matches = str =~ Number.getRegex()
+			def matches = str =~ ~"^[0-9.]+"
 			if (matches.size() == 1) {
-				consumeToken(new Number(Integer.parseInt(matches[0])))
+				consumeToken(new Number(Double.parseDouble(matches[0])))
 				str = str.substring(matches[0].size())
 			} else {
 				// skipping whitespace
@@ -100,80 +138,49 @@ class ArithmeticParser extends LRParser {
 		str
 	}
 
-	def ArithmeticParser() {
-		tokens = [
-			new Operator() {
-				int getPrecedence() { 1 }
-				String getRegex() { ~/^\+/ }
-				Object action(x, y) { x + y }
-			},
-			new Operator() {
-				int getPrecedence() { 1 }
-				String getRegex() { ~/^\-/ }
-				Object action(x, y) { x - y }
-			},
-			new Operator() {
-				int getPrecedence() { 2 }
-				String getRegex() { ~/^\*/ }
-				Object action(x, y) { x * y }
-			},
-			new Operator() {
-				int getPrecedence() { 2 }
-				String getRegex() { ~/^\// }
-				Object action(x, y) { x / y }
-			}
-		]
+	void additonalTokens() {
+		operators << new BinaryOperator(1, ~"^\\+", { x, y -> x + y })
+		operators << new BinaryOperator(1, ~"^-",   { x, y -> x - y })
+		operators << new BinaryOperator(2, ~"^\\*", { x, y -> x * y })
+		operators << new BinaryOperator(2, ~"^/",   { x, y -> x / y })
 	}
 }
 
+assert new ArithmeticParser().parse("2 * 3 + 4 / 5") == 6.8
+assert new ArithmeticParser().parse("2 * (3 + 4) / 5") == 2.8
+
+/* = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
 
 class CompilingArithmeticParser extends ArithmeticParser {
 
-	class BinaryOp {
-		Object left, right
-		String name
-		String toString() {
-			'(' + left + ' ' + name + ' ' + right + ')'
+	class BinaryOperator extends ArithmeticParser.BinaryOperator {
+		BinaryOperator(int precedence, Pattern regex, String name) {
+			super(precedence, regex, {x, y ->
+				ParseTreeNode node = new ParseTreeNode()
+				node.left = x
+				node.right = y
+				node.name = name
+				node
+			})
 		}
-		BinaryOp(x, y, op) {
-			left = x
-			right = y
-			name = op
+
+		static class ParseTreeNode {
+			Object left, right
+			String name
+			String toString() { '(' + left + ' ' + name + ' ' + right + ')' }
 		}
 	}
 
-	def CompilingArithmeticParser() {
-		tokens = [
-			new Operator() {
-				String toString() { "+" }
-				String getRegex() { ~/^\+/ }
-				int getPrecedence() { 1 }
-				Object action(x, y) { new BinaryOp(x, y, '+') }
-			},
-			new Operator() {
-				String toString() { "-" }
-				String getRegex() { ~/^\-/ }
-				int getPrecedence() { 1 }
-				Object action(x, y) { new BinaryOp(x, y, '-') }
-			},
-			new Operator() {
-				String toString() { "*" }
-				String getRegex() { ~/^\*/ }
-				int getPrecedence() { 2 }
-				Object action(x, y) { new BinaryOp(x, y, '*') }
-			},
-			new Operator() {
-				String toString() { "/" }
-				String getRegex() { ~/^\// }
-				int getPrecedence() { 2 }
-				Object action(x, y) { new BinaryOp(x, y, '/') }
-			}
-		]
+	void additonalTokens() {
+		operators << new BinaryOperator(1, ~"^\\+", "+")
+		operators << new BinaryOperator(1, ~"^-",   "-")
+		operators << new BinaryOperator(2, ~"^\\*", "*")
+		operators << new BinaryOperator(2, ~"^/",   "/")
 	}
 }
 
 
-def p = new CompilingArithmeticParser();
-// def p = new ArithmeticParser();
-
-println(p.parse("2*3 + 4/5"))
+assert new CompilingArithmeticParser().parse("2 * 3 + 4 / 5").toString() ==
+	'((2.0 * 3.0) + (4.0 / 5.0))'
+assert new CompilingArithmeticParser().parse("2 * (3 + 4) / 5").toString() ==
+	'((2.0 * (3.0 + 4.0)) / 5.0)'
